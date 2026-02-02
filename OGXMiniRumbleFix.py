16bd153ec998b96806c587e328e852af96eaa648
Rachel Bartmoss <56975081+faithvoid@users.noreply.github.com>
2026-02-02 03:59:48 -0500

    Update OGXMiniRumbleFix.py

--- OGXMiniRumbleFix.py
+++ OGXMiniRumbleFix.py
@@ -1,13 +1,12 @@
-import usb.core
-import usb.util
-import time
-import sys
+import usb.core, usb.util, time, sys, os, webbrowser
 
 VID = 0x045E # Microsoft VID, may require changing for third-party controllers.
 PID_WIRED = 0x028E # Genuine wired Xbox 360 controller PID
 PID_W = 0x0719 # Genuine Xbox 360 wireless adapter PID
 PID_GENERIC = 0x0291 # Third-party wireless adapter PID
+PID_GENERIC2 = 0x02AA # Additional third-party wireless adapter PID
 PID = 0x0289 # Third-party(?) Xbox 360 controller PID
+GAMEPAD_URL = "https://hardwaretester.com/gamepad"
 
 RUMBLE_ENABLE = bytes([0x00, 0x00, 0x08, 0x01])
 
@@ -38,7 +37,7 @@ by [''' +
 
 ''')
 
-    dev = usb.core.find(idVendor=VID, idProduct=PID) or usb.core.find(idVendor=VID, idProduct=PID_GENERIC) or usb.core.find(idVendor=VID, idProduct=PID_W) or usb.core.find(idVendor=VID, idProduct=PID_WIRED)
+    dev = usb.core.find(idVendor=VID, idProduct=PID) or usb.core.find(idVendor=VID, idProduct=PID_GENERIC) or usb.core.find(idVendor=VID, idProduct=PID_GENERIC2) or usb.core.find(idVendor=VID, idProduct=PID_W) or usb.core.find(idVendor=VID, idProduct=PID_WIRED)
     if dev is None:
         print("Xbox 360 controller / wireless receiver not found! Please reconnect your controller and try again.")
         print("If that doesn't work, find the VID and PID of your controller and replace PID and VID values in the script and retry!\n")
@@ -73,8 +72,15 @@ by [''' +
     usb.util.release_interface(dev, intf_num)
     usb.util.dispose_resources(dev)
 
-    print("Xbox 360 controller rumble should be fixed! Unplug + Replug the controller and visit the URL below to test vibration:\n")
-    print("https://hardwaretester.com/gamepad" + "\n")
+    time.sleep(1)
+
+    if os.geteuid() == 0:
+        print("Xbox 360 controller rumble should be fixed! Unplug + Replug the controller and visit the URL below to test vibration:\n")
+        print("https://hardwaretester.com/gamepad" + "\n")
+
+    else:
+        print("Xbox 360 controller rumble should be fixed! Unplug + Replug the controller check your vibration in the browser window!")
+        webbrowser.open(GAMEPAD_URL)
 
 if __name__ == "__main__":
     main()
